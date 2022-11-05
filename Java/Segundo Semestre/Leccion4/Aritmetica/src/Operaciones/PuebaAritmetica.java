61c1e1e7025bfbd724aeb3bca9acddaf22312e68
Matias Esequiel Carballo <matiasesek1995@gmail.com>
2022-11-05 13:24:32 -0300

    Actualizo las clases de Java para estar al día

--- Java/Segundo Semestre/Leccion4/Aritmetica/src/Operaciones/PuebaAritmetica.java
+++ Java/Segundo Semestre/Leccion4/Aritmetica/src/Operaciones/PuebaAritmetica.java
@@ -43,7 +43,22 @@ class Persona{
     String apellido;
     
     Persona(String nombre, String apellido){ //Constructor
+        super(); //Llamada al constructor de la clase Padre object
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
         this.nombre = nombre;
         this.apellido = apellido;
+        System.out.println("Objeto persona usando this: "+this);
+    }
+}
+
+class Imprimir{
+    public Imprimir(){
+        super(); //el constructor de la clase padre, para reservar memoria
+    }
+    
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde la clase imprimir: "+persona);
+        System.out.println("Impresión del objeto actual (this): "+this);
     }
 }

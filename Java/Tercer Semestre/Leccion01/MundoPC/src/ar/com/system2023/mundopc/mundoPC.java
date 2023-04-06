112f5e35ed8fa58f5bf91fe602182caa4d11a549
lokywolf2295 <matiasesek1995@gmail.com>
2023-04-05 23:50:34 -0300

    Update: Add exercise to  mundoPC class in MundoPC project

--- Java/Tercer Semestre/Leccion01/MundoPC/src/ar/com/system2023/mundopc/mundoPC.java
+++ Java/Tercer Semestre/Leccion01/MundoPC/src/ar/com/system2023/mundopc/mundoPC.java
@@ -31,5 +31,65 @@ public class mundoPC {
         orden2.agregarCompuradora(computadorasVarias);
         //mostramos la lista de ordenes.
         orden2.mostrarOrden();
- }
+
+
+        //Crear mas objetos de tipo computadora con todos sus elementos
+        //completar una lista en el objeto orden1 que llegue a los 10 elementos
+        //probar de esta manera los métodos al maximo rendimiento
+
+        //Creamos los objetos de cada clase
+        Monitor monitorSamsung = new Monitor("Samsung", 13);
+        Teclado tecladoSamsung = new Teclado("Bluetooth", "Samsung");
+        Raton ratonSamsung = new Raton("Bluetooth", "Samsung");
+        Computadora computadoraSamsung = new Computadora("ComputadoraSamsung",monitorSamsung,tecladoSamsung,ratonSamsung);
+
+        Monitor monitorTokio = new Monitor("Tokio", 13);
+        Teclado tecladoTokio = new Teclado("Bluetooth", "Tokio");
+        Raton ratonTokio = new Raton("Bluetooth", "Tokio");
+        Computadora computadoraTokio = new Computadora("ComputadoraTokio",monitorTokio,tecladoTokio,ratonTokio);
+
+        Monitor monitorHitachi = new Monitor("Hitachi", 13);
+        Teclado tecladoHitachi = new Teclado("Bluetooth", "Hitachi");
+        Raton ratonHitachi = new Raton("Bluetooth", "Hitachi");
+        Computadora computadoraHitachi = new Computadora("ComputadoraHitachi",monitorHitachi,tecladoHitachi,ratonHitachi);
+
+        Monitor monitorSnapDragon = new Monitor("SnapDragon", 13);
+        Teclado tecladoSnapDragon = new Teclado("Bluetooth", "SnapDragon");
+        Raton ratonSnapDragon = new Raton("Bluetooth", "SnapDragon");
+        Computadora computadoraSnapDragon = new Computadora("ComputadoraSnapDragon",monitorSnapDragon,tecladoSnapDragon,ratonSnapDragon);
+
+        Monitor monitorNSX = new Monitor("NSX", 13);
+        Teclado tecladoNSX = new Teclado("Bluetooth", "NSX");
+        Raton ratonNSX = new Raton("Bluetooth", "NSX");
+        Computadora computadoraNSX = new Computadora("ComputadoraNSX",monitorNSX,tecladoNSX,ratonNSX);
+
+        Monitor monitorAsus = new Monitor("Asus", 13);
+        Teclado tecladoAsus = new Teclado("Bluetooth", "Asus");
+        Raton ratonAsus = new Raton("Bluetooth", "Asus");
+        Computadora computadoraAsus = new Computadora("ComputadoraAsus",monitorAsus,tecladoAsus,ratonAsus);
+
+        Monitor monitorLogitech = new Monitor("Logitech", 13);
+        Teclado tecladoLogitech = new Teclado("Bluetooth", "Logitech");
+        Raton ratonLogitech = new Raton("Bluetooth", "Logitech");
+        Computadora computadoraLogitech = new Computadora("ComputadoraLogitech",monitorLogitech,tecladoLogitech,ratonLogitech);
+
+        //Agregamos las nuevas computadoras a la orden uno para cumplir con la consigna
+        orden1.agregarCompuradora(computadorasVarias);
+        orden1.agregarCompuradora(computadoraSamsung);
+        orden1.agregarCompuradora(computadoraTokio);
+        orden1.agregarCompuradora(computadoraHitachi);
+        orden1.agregarCompuradora(computadoraSnapDragon);
+        orden1.agregarCompuradora(computadoraNSX);
+        orden1.agregarCompuradora(computadoraAsus);
+        orden1.agregarCompuradora(computadoraLogitech);
+
+        //mostramos la lista de ordenes llena.
+        orden1.mostrarOrden();
+
+        //Agregamos una pc mas para probar el maximo de la orden
+        orden1.agregarCompuradora(computadorasVarias);
+
+        //mostramos la lista de ordenes llena y no se agrega la ultima pc ya que superó el maximo.
+        orden1.mostrarOrden();
+    }
 }

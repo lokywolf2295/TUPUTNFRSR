a73275713e3f9bbc274171d0c0b39321984786c3
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-03 23:36:52 -0300

    Update(Java): Add Continentes enum and call Continentes methods in TestEnumeraciones class

--- Java/Tercer Semestre/Leccion02/Enumeraciones/src/main/java/ar/com/utnfrsr/test/TestEnumeraciones.java
+++ Java/Tercer Semestre/Leccion02/Enumeraciones/src/main/java/ar/com/utnfrsr/test/TestEnumeraciones.java
@@ -7,6 +7,31 @@ public class TestEnumeraciones {
         System.out.println("Dia 1: " + Dias.LUNES);
         indicarDiaSemana(Dias.LUNES);//Las enumeraciones se tratan como cadenas
         //ahora no se deben utilizar comillas, se accede a traves del operador de punto
+        System.out.println("Continente no. 1: " + ar.com.utnfrsr.enumeraciones.Continentes.AFRICA);
+        System.out.println("Nro. de paises en el 1er continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.AFRICA.getPaises());
+        System.out.println("Nro. de habitantes en el 1er continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.AFRICA.getHabitantes());
+        System.out.println("Continente no. 2: " + ar.com.utnfrsr.enumeraciones.Continentes.ASIA);
+        System.out.println("Nro. de paises en el 2do continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.ASIA.getPaises());
+        System.out.println("Nro. de habitantes en el 2do continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.ASIA.getHabitantes());
+        System.out.println("Continente no. 3: " + ar.com.utnfrsr.enumeraciones.Continentes.EUROPA);
+        System.out.println("Nro. de paises en el 3er continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.EUROPA.getPaises());
+        System.out.println("Nro. de habitantes en el 3er continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.EUROPA.getHabitantes());
+        System.out.println("Continente no. 4: " + ar.com.utnfrsr.enumeraciones.Continentes.AMERICA);
+        System.out.println("Nro. de paises en el 4to continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.AMERICA.getPaises());
+        System.out.println("Nro. de habitantes en el 4to continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.AMERICA.getHabitantes());
+        System.out.println("Continente no. 5: " + ar.com.utnfrsr.enumeraciones.Continentes.OCEANIA);
+        System.out.println("Nro. de paises en el 5to continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.OCEANIA.getPaises());
+        System.out.println("Nro. de habitantes en el 5to continente: "
+                + ar.com.utnfrsr.enumeraciones.Continentes.OCEANIA.getHabitantes());
     }
 
     private static void indicarDiaSemana(Dias dias){

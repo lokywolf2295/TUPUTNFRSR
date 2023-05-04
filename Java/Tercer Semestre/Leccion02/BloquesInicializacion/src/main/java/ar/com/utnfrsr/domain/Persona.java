dff4a6d1bab11db3a152b4542fecaca143287720
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-03 23:56:37 -0300

    Update(Java): Add BloquesInicializacion project in Leccion02 folder

---
+++ Java/Tercer Semestre/Leccion02/BloquesInicializacion/src/main/java/ar/com/utnfrsr/domain/Persona.java
@@ -0,0 +1,26 @@
+package ar.com.utnfrsr.domain;
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+
+    static { // Bloquede inicialización estatico
+        System.out.println("Ejecucion bloque estatico");
+        ++Persona.contadorPersonas;
+        //idPersona = 10; // No se puede acceder a un atributo no estatico desde un contexto estatico
+    }
+
+    { // Bloque de inicialización no estatico (Contexto dinamico)
+        System.out.println("Ejecucion bloque no estatico");
+        this.idPersona = Persona.contadorPersonas++; //incrementamos el atributo contador para asignar a la persona actual
+    }
+
+    //Los bloques de inicialización se ejecutan antes del constructor de la clase
+    public Persona() {
+        System.out.println("Ejecucion del constructor");
+    }
+
+    public int getIdPersona() {
+        return this.idPersona;
+    }
+}

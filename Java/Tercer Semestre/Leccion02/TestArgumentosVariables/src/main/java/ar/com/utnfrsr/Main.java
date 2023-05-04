18ad8a042621505eb9c21153fac98b8eb7150998
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-03 21:51:02 -0300

    Update(Java): Add variosParametros method in Main class

--- Java/Tercer Semestre/Leccion02/TestArgumentosVariables/src/main/java/ar/com/utnfrsr/Main.java
+++ Java/Tercer Semestre/Leccion02/TestArgumentosVariables/src/main/java/ar/com/utnfrsr/Main.java
@@ -2,7 +2,14 @@ package ar.com.utnfrsr;
 
 public class Main {
     public static void main(String[] args) {
+        imprimirNumeros(3,4,5);
+        imprimirNumeros(1,2);
+        variosParametros("Juan", "Perez", 7,8,9);
+    }
 
+    private static void variosParametros(String nombre, String apellido, int... numeros){
+        System.out.println("Nombre = " + nombre + ", Apellido = " + apellido);
+        imprimirNumeros(numeros);
     }
 
     private static void imprimirNumeros(int ...numeros){//los tres puntos representan argumentos variables.

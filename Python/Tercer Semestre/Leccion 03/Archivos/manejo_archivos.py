4f90d5c27e13526d44a66635ad91c872ca78e973
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-06 15:35:58 -0300

    update(Python): add content in the file test.txt from the project file_management.py

--- Python/Tercer Semestre/Leccion 03/Archivos/manejo_archivos.py
+++ Python/Tercer Semestre/Leccion 03/Archivos/manejo_archivos.py
@@ -1,8 +1,14 @@
 # Declaramos una variable
 
 try:
-    archivo = open("prueba.txt", "w")  # la w es para hacer referencia a Write (escribir)
+    archivo = open("prueba.txt", "w", encoding='utf8')  # la w es para hacer referencia a Write (escribir)
+    archivo.write("Programamos con diferentes tipos de archivos, ahora en txt\n")
+    archivo.write("Los acentos son importantes para las palabras\n")
+    archivo.write(
+        "como por ejemplo: acción, ejecución y producción\n")  # esta linea al pasarse al txt ocaciona error por falta del encoding
+    archivo.write("Con esto terminamos")
 except Exception as e:
     print(e)
-finally: # el finally se ejecuta siempre, haya o no haya error
-    archivo.close() # cerramos el archivo
+finally:  # el finally se ejecuta siempre, haya o no haya error
+    archivo.close()  # cerramos el archivo
+# archivo.write('Todo quedo perfecto'): esto es un error porque el archivo ya esta cerrado

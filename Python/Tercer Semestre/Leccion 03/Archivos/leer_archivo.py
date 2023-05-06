1292c8ece1efd22854f0a65afc0eea9856273c1c
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-06 16:12:37 -0300

    update(Python): add new methods to print in the project leer_archivo.py

--- Python/Tercer Semestre/Leccion 03/Archivos/leer_archivo.py
+++ Python/Tercer Semestre/Leccion 03/Archivos/leer_archivo.py
@@ -1,6 +1,10 @@
 try:
     archivo = open('prueba.txt', 'r', encoding='utf8')  # la r es para hacer referencia a Read (leer)
-    print(archivo.read())  # lee la totalidad del archivo
+    # print(archivo.read())  # lee la totalidad del archivo
+    print(archivo.read(16))  # lee los primeros 16 caracteres del archivo
+    print(archivo.read(10))  # lee los siguientes 10 caracteres del archivo
+    print(archivo.readline())  # lee la primera linea del archivo continuando con el texto si ya tiene un read
+    print(archivo.readline())  # lee la segunda linea del archivo continuando con el texto.
 except Exception as e:
     print(e)
 finally:

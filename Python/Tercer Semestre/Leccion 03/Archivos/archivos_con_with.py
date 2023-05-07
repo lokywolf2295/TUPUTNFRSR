1b8957eda8eab3b1802065501163013a82a8a0cc
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-06 22:46:31 -0300

    update(Python): add context manager method in the project archivos_con_with.py

---
+++ Python/Tercer Semestre/Leccion 03/Archivos/archivos_con_with.py
@@ -0,0 +1,7 @@
+# MANEJO DE CONTEXTOS CON WITH (CONTEXT MANAGER): sintaxis simplificada, abre y cierra el archivo
+with open('prueba.txt','r', encoding='utf8') as archivo: # con with no es necesario cerrar el archivo, ya que lo hace automaticamente
+    print(archivo.read())  # lee la totalidad del archivo
+# No hace falta mi el try, except y finally
+# En el contexto de with lo que se ejecuta de manera automatica
+# Utiliza diferentes métodos: __enter__ este es el que se ejecuta al inicio y abre el archivo
+# Ahora el siguiente metodo es: __exit__ este es el que se ejecuta al final y cierra el archivo

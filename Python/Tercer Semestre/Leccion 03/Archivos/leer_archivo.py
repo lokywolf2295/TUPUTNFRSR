e8d103eeebc7c3af17664222e49a59615b79ec3d
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-06 21:41:52 -0300

    update(Python): add the copy method using copia.txt file in the project leer_archivo.py

--- Python/Tercer Semestre/Leccion 03/Archivos/leer_archivo.py
+++ Python/Tercer Semestre/Leccion 03/Archivos/leer_archivo.py
@@ -7,11 +7,19 @@ try:
     # print(archivo.readline())  # lee la segunda linea del archivo continuando con el texto.
 
     # vamos a iterar el archivo, cad una de las lineas
-    #for linea in archivo:
+    # for linea in archivo:
         # print(linea): iteramos todos los elementos del archivo
 
-    print(archivo.readlines()) #accedemos al archivo como si fuera una lista, imprimiendo todas sus lineas
+    #print(archivo.readlines()) # accedemos al archivo como si fuera una lista, imprimiendo todas sus lineas
+
+    # Anexamos informacion, copiamos el archivo prueba.txt en otro
+    archivo2 = open('copia.txt', 'a', encoding='utf8')
+    archivo2.write(archivo.read())  # copiamos el archivo prueba.txt en otro
+
 except Exception as e:
     print(e)
 finally:
-    archivo.close()  # cerramos el archivo
+    archivo.close()  # cerramos el primer archivo
+    archivo2.close()  # cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiar archivos')

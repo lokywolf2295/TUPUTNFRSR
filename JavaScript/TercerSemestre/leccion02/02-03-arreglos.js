d01f3b65018f06e79ec8be9f9aff87b69ce7a4cf
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-08 20:40:25 -0300

    Update(JavaScript): add array in leccion02 folder

---
+++ JavaScript/TercerSemestre/leccion02/02-03-arreglos.js
@@ -0,0 +1,4 @@
+//let autos = new Array('BMW', 'Mercedes Benz', 'Volvo'); // Forma antigua de declarar un arreglo (array)
+
+const autos = ['Ferrari', 'Renault', 'BMW']; // Forma moderna de declarar un arreglo (array)
+console.log(autos);

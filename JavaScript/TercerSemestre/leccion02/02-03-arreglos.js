8fc7dab36e0e20a6f7633ac29d484b6642cade81
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-08 20:45:12 -0300

    Update(JavaScript): add traverse an array

--- JavaScript/TercerSemestre/leccion02/02-03-arreglos.js
+++ JavaScript/TercerSemestre/leccion02/02-03-arreglos.js
@@ -2,3 +2,12 @@
 
 const autos = ['Ferrari', 'Renault', 'BMW']; // Forma moderna de declarar un arreglo (array)
 console.log(autos);
+
+//Recorremos los elementos del arreglo de manera manual
+console.log(autos[0]);
+console.log(autos[2]);
+
+//Recorremos los elementos del arreglo de manera automática
+for (let i = 0; i < autos.length; i++) {
+    console.log('Indice '+i+': '+autos[i]);
+}

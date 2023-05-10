787dd91ca0a0b061dd8ab7842197132698c3ad2b
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-09 23:11:03 -0300

    update(JavaScript): add function explanations as objects

--- JavaScript/TercerSemestre/leccion03/01-03-funciones.js
+++ JavaScript/TercerSemestre/leccion03/01-03-funciones.js
@@ -26,3 +26,17 @@ console.log(resultado);
 (function (a, b) { //este tipo de funciones no puede ser llamada mas de una vez ya que no fué asignada a una variable
     console.log('Ejecutando la función: ' + (a + b));
 })(3, 4); //necesita cierre con punto y coma
+
+console.log(typeof miFuncion); //imprime el tipo de dato de la variable
+
+//Funciones como objetos
+function miFuncion3(a, b) {
+    console.log(arguments); //imprime la cantidad de argumentos que recibe la función
+}//las funciones son objetos por eso podemos utilizar el punto para acceder a sus propiedades
+// como por ejemplo arguments.length que reemplaza el codigo anterior
+
+miFuncion3(2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+//toString() convierte un objeto a string
+var miFuncionTexto = miFuncion.toString();
+console.log(miFuncionTexto);

96cd3b2501166df1966fba8d2abb3342b75936d7
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-09 22:52:08 -0300

    update(JavaScript): add an anonymous or expression function

--- JavaScript/TercerSemestre/leccion03/01-03-funciones.js
+++ JavaScript/TercerSemestre/leccion03/01-03-funciones.js
@@ -16,3 +16,8 @@ function miFuncion2(a, b) {
 
 let resultado = miFuncion2(2, 9);
 console.log(resultado);
+
+//Funciones de tipo expresión o anónima
+let x = function (a, b) {return a + b}; //necesita cierre con punto y coma
+resultado = x(5,6) //al llamarla se pone la variable y paréntesis
+console.log(resultado);

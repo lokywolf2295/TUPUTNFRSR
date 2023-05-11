d3492737201245a0e647412fe5609eab0b540685
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-11 00:13:12 -0300

    Update(Java): add the wrap-around class in the project AutoboxingUnboxing

--- Java/Tercer Semestre/Leccion03/AutoboxingUnboxing/src/main/java/ar/com/utnfrsr/test/TestAutoboxingUnboxing.java
+++ Java/Tercer Semestre/Leccion03/AutoboxingUnboxing/src/main/java/ar/com/utnfrsr/test/TestAutoboxingUnboxing.java
@@ -6,11 +6,20 @@ public class TestAutoboxingUnboxing {
         /*
             Clases envolventes de tipos primitivos
             int = la clase envolvente es Integer
+            long = la clase envolvente es Long
+            float = la clase envolvente es Float
+            double = la clase envolvente es Double
+            boolean = la clase envolvente es Boolean
+            byte = la clase envolvente es Byte
+            char = la clase envolvente es Character
+            short = la clase envolvente es Short
          */
         int enteroPrim = 10; //Tipo primitivo
         System.out.println("enteroPrim = " + enteroPrim);
         Integer entero = 10; //Clase envolvente (Tipo objet de la clase Integer)
-        System.out.println("entero = " + entero.toString());
+        System.out.println("entero = " + entero.doubleValue()); //Autoboxing (Se convierte de tipo primitivo a objeto)
+
+        int entero2 = entero; //Unboxing (Se convierte de objeto a tipo primitivo)
     }
 
 

0e9e5893ac7a46aafc0af47bd4fbb7282f945997
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-11 22:56:56 -0300

    update(Java): add in class TestDefault the ClaseHija2  to ModificadoresAcceso project

--- Java/Tercer Semestre/Leccion03/ModificacoresAcceso/src/main/java/ar/com/utnfrsr/paquete1/TestDefault.java
+++ Java/Tercer Semestre/Leccion03/ModificacoresAcceso/src/main/java/ar/com/utnfrsr/paquete1/TestDefault.java
@@ -2,6 +2,9 @@ package ar.com.utnfrsr.paquete1;
 
 public class TestDefault {
     public static void main(String[] args) {
-        Clase2 clase2 = new Clase2();
+        //Clase2 clase2 = new Clase2();
+        ClaseHija2 claseHija2 = new ClaseHija2();
+        claseHija2.atributoDefault = "Cambio desde la Prueba";
+        System.out.println("Clase Hija atributo default = " + claseHija2.atributoDefault);
     }
 }

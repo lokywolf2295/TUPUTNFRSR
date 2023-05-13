f736f09743bacc5aa162d9b4ddadca23e90f1df5
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-12 23:04:47 -0300

    update(JavaScript): add the file 01-04-objets.js in the folder leccion04

---
+++ JavaScript/TercerSemestre/leccion04/01-04-objetos.js
@@ -0,0 +1,12 @@
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
+
+//Objeto
+let persona = { //se le asigna un espacio en memoria al objeto
+    nombre: "Juan",
+    apellido: "Perez",
+    email: "jperez@gmail.com",
+    edad: 28
+}
+
+console.log(persona.nombre);

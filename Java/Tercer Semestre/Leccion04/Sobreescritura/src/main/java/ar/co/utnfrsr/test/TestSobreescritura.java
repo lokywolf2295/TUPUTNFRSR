47984aac7c7678b36fee0a0943b9b30600e08f48
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-16 20:48:10 -0300

    Update(Java): apply polymorphism to the TestOverwrite class

--- Java/Tercer Semestre/Leccion04/Sobreescritura/src/main/java/ar/co/utnfrsr/test/TestSobreescritura.java
+++ Java/Tercer Semestre/Leccion04/Sobreescritura/src/main/java/ar/co/utnfrsr/test/TestSobreescritura.java
@@ -1,10 +1,18 @@
 package ar.co.utnfrsr.test;
 
-import ar.co.utnfrsr.domain.Gerente;
+import ar.co.utnfrsr.domain.*;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Jose", 10000);
+        imprimir(empleado1); //aplicamos polimorfirmo pasandole a un mismo metodo un tipo de dato
+        //System.out.println("Empleado1 = "+empleado1.obtenerDetalles());
         Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
-        System.out.println("Gerente1 = "+gerente1.obtenerDetalles());
+        imprimir(gerente1);//aplicamos polimorfirmo pasandole a otro mismo metodo un tipo de dato diferente
+        //System.out.println("Gerente1 = "+gerente1.obtenerDetalles());
+    }
+
+    public static void imprimir(Empleado empleado) { //la referencia que se recibe (empleado) es la apliación de herencia
+        System.out.println("empleado = " + empleado.obtenerDetalles());
     }
 }

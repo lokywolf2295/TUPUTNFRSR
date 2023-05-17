3678da1c31e68b9672c08d6844be9394c2af4058
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-16 22:34:26 -0300

    Update(Java): add method to casting variables in the project InstanceOf

--- Java/Tercer Semestre/Leccion04/InstanceOf/src/main/java/ar/co/utnfrsr/test/TestInstanceOf.java
+++ Java/Tercer Semestre/Leccion04/InstanceOf/src/main/java/ar/co/utnfrsr/test/TestInstanceOf.java
@@ -13,6 +13,7 @@ public class TestInstanceOf {
     public static void determinarTipo(Empleado empleado) {
         if (empleado instanceof Gerente){
             System.out.println("Es de tipo Gerente");
+            ((Gerente) empleado).getDepartamento(); //casting convertimos la variable padre a una variable hija (Empleado a Gerente)
         } else if (empleado instanceof Empleado) {
             System.out.println("Es de tipo Empleado");
         } else if (empleado instanceof Object) {

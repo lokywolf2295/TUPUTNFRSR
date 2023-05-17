031ad006224ef25eab04ec350be8670cc49a29c9
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-16 21:16:12 -0300

    Update(Java): add the project InstanceOf in the folder Leccion04

---
+++ Java/Tercer Semestre/Leccion04/InstanceOf/src/main/java/ar/co/utnfrsr/test/TestInstanceOf.java
@@ -0,0 +1,22 @@
+package ar.co.utnfrsr.test;
+
+import ar.co.utnfrsr.domain.*;
+
+public class TestInstanceOf {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Jose", 10000);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("Juan", 5000, "Sistemas");
+        determinarTipo(empleado1);
+     }
+
+    public static void determinarTipo(Empleado empleado) {
+        if (empleado instanceof Gerente){
+            System.out.println("Es de tipo Gerente");
+        } else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+        } else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
+    }
+}

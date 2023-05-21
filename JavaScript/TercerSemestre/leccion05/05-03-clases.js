8cdbd6c99739e2fe877f38e0472d822aa044b11d
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-20 23:58:05 -0300

    Update(JavaScript): add set method in the class Persona

--- JavaScript/TercerSemestre/leccion05/05-03-clases.js
+++ JavaScript/TercerSemestre/leccion05/05-03-clases.js
@@ -4,14 +4,37 @@ class Persona{ //por convención se usa mayúscula en el nombre de la clase
         this._apellido = apellido;
     }
     
+    //Métodos get y set
     get nombre(){
         return this._nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
     }
 }
 
 let persona1 = new Persona("Juan", "Perez");
 console.log(persona1.nombre);
+persona1.nombre = "Juan Carlos";
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.apellido = "Ramos";
+console.log(persona1.apellido);
 //console.log(persona1);
 let persona2 = new Persona("Carlos", "Lara");
 console.log(persona2.nombre);
+persona2.nombre = "María Laura";
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+persona2.apellido = "Quintero";
+console.log(persona2.apellido);
 //console.log(persona2);

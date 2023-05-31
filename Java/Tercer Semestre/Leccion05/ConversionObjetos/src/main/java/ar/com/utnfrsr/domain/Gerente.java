a84cff5a3eb61a28ba14feab0f6ed4b3adda52ae
lokywolf2295 <matiasesek1995@gmail.com>
2023-05-30 23:14:09 -0300

    Update(Java): 1 add the project ConversionObjetos in the folder leccion05

---
+++ Java/Tercer Semestre/Leccion05/ConversionObjetos/src/main/java/ar/com/utnfrsr/domain/Gerente.java
@@ -0,0 +1,16 @@
+package ar.com.utnfrsr.domain;
+
+public class Gerente extends Empleado{
+    private String departamento;
+
+    public Gerente(String nombre, double sueldo, String departamento) {
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    }
+
+    //Sobreescritura del metodo padre
+    @Override
+    public String obtenerDetalles() {
+        return super.obtenerDetalles() + ", Departamento: " + this.departamento;
+    }
+}

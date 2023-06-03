376f11eee3a26a912226a7a214ca68c9a2c6172d
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-03 20:17:40 -0300

    Update(Java): 6 add imprimir method

--- Java/Tercer Semestre/Leccion06/Interfaces/src/main/java/ar/com/utnfrsr/test/TestInterfaces.java
+++ Java/Tercer Semestre/Leccion06/Interfaces/src/main/java/ar/com/utnfrsr/test/TestInterfaces.java
@@ -6,6 +6,15 @@ public class TestInterfaces {
     public static void main(String[] args) {
         //IAccesoDatos datos = new IAccesoDatos(); //No se puede instanciar una interfaz
         IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        //imprimir(datos);
+
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+
+    public static void imprimir(IAccesoDatos datos) {
         datos.listar();
     }
 }

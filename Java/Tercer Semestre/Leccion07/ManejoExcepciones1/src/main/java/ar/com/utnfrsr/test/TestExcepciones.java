7b86c52f71056fac3c18adaa26ab42a35dbfb617
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-04 09:33:24 -0300

    Update(Java): 6 remove try and catch methods

--- Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
+++ Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
@@ -3,12 +3,12 @@ package ar.com.utnfrsr.test;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-        try {
+        //try {
             resultado = 10/0; //Esto lanzará una ArithmeticException (división por cero) en la linea 5
-        } catch (ArithmeticException e) {
-            System.out.println("Ocurrió una excepción: ");
-            e.printStackTrace(System.out); //Imprime el stack trace de la excepción (se conoce como la pila de excepciones)
-        }
+        //} catch (ArithmeticException e) {
+        //    System.out.println("Ocurrió una excepción: ");
+        //    e.printStackTrace(System.out); //Imprime el stack trace de la excepción (se conoce como la pila de excepciones)
+        //}
         System.out.println("La variable de resultado tiene como valor: " + resultado);
     }
 }

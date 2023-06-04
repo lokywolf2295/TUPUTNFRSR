fc019d122fdf7dd35f9ca74464fca33c09d3e2be
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-04 11:55:44 -0300

    Update(Java): 9 test the division method in the test package

--- Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/aritmetica/Aritmetica.java
+++ Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/aritmetica/Aritmetica.java
@@ -1,9 +1,11 @@
 package ar.com.utnfrsr.aritmetica;
 
+import ar.com.utnfrsr.excepciones.OperacionExcepcion;
 public class Aritmetica {
-    public static int division(int numerador, int denominador) throws Exception {
+    public static int division(int numerador, int denominador)
+            throws OperacionExcepcion {
         if (denominador == 0) {
-            throw new Exception("No se puede dividir por cero");
+            throw new OperacionExcepcion("No se puede dividir por cero");
         }
         return numerador / denominador;
     }
--- Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
+++ Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
@@ -1,13 +1,15 @@
 package ar.com.utnfrsr.test;
 
+import static ar.com.utnfrsr.aritmetica.Aritmetica.division;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         try {
-            resultado = 10/0; //Esto lanzará una ArithmeticException (división por cero) en la linea 5
-        } catch (ArithmeticException e) {
+            resultado = division(10,0); //Esto lanzará una ArithmeticException (división por cero) en la linea 5
+        } catch (Exception e) {
             System.out.println("Ocurrió una excepción: ");
             e.printStackTrace(System.out); //Imprime el stack trace de la excepción (se conoce como la pila de excepciones)
+            System.out.println(e.getMessage());
         }
         System.out.println("La variable de resultado tiene como valor: " + resultado);
     }

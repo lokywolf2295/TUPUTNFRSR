930dc348ae2db17d2ec755078125e0a94977d125
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-04 09:26:28 -0300

    Update(Java): 4 add try and catch methods

--- Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
+++ Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/test/TestExcepciones.java
@@ -2,7 +2,13 @@ package ar.com.utnfrsr.test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        int resultado = 10/0; //Esto lanzará una ArithmeticException (división por cero) en la linea 5
-        System.out.println(resultado);
+        int resultado = 0;
+        try {
+            resultado = 10/0; //Esto lanzará una ArithmeticException (división por cero) en la linea 5
+        } catch (ArithmeticException e) {
+            System.out.println("Ocurrió una excepción: ");
+            e.printStackTrace(System.out); //Imprime el stack trace de la excepción (se conoce como la pila de excepciones)
+        }
+        System.out.println("La variable de resultado tiene como valor: " + resultado);
     }
 }

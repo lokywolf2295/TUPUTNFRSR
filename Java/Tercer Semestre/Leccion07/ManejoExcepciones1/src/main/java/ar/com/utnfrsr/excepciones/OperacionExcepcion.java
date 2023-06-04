7fe6c0d54ff95a58bd7c82a26c7f0583ca404ce4
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-04 12:14:19 -0300

    Update(Java): 10 add the RunTimeException exception in the OperacionExcepcion class

--- Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/excepciones/OperacionExcepcion.java
+++ Java/Tercer Semestre/Leccion07/ManejoExcepciones1/src/main/java/ar/com/utnfrsr/excepciones/OperacionExcepcion.java
@@ -1,6 +1,6 @@
 package ar.com.utnfrsr.excepciones;
 
-public class OperacionExcepcion extends Exception{
+public class OperacionExcepcion extends RuntimeException{
     //Constructor
     public OperacionExcepcion(String mensaje){
         super(mensaje);

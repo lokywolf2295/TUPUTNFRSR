147eec61e2d6d7d90198e916b753266ce42da261
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-08 00:08:25 -0300

    Update(Java): 4 add the Menu and if method to the CalculadoraUTN class

--- Java/Tercer Semestre/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Tercer Semestre/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -4,11 +4,22 @@ public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("****** Aplicación Calculadora ******");
-        System.out.print("Digite el calor para el operando1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el calor para el operando2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("Resultado = " + resultado);
+        //Mostramos el Menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. División
+                5. Salir
+                """);
+        System.out.print("Operación a Realizar: ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if (operacion >= 1 && operacion <= 4) {
+            System.out.print("Digite el Valor para el operando1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Digite el Valor para el operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+        }
     }
 }

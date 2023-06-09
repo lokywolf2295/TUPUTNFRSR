55136c9919171eea5952880bc22114ec93395695
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-08 22:36:18 -0300

    Update(Java): 5 add the Switch method to the CalculadoraUTN class

--- Java/Tercer Semestre/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Tercer Semestre/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -20,6 +20,32 @@ public class CalculadoraUTN {
             var operando1 = Integer.parseInt(entrada.nextLine());
             System.out.print("Digite el Valor para el operando2: ");
             var operando2 = Integer.parseInt(entrada.nextLine());
+
+            int resultado;
+            switch (operacion){
+                case 1 -> {//Suma
+                    resultado = operando1 + operando2;
+                    System.out.println("El resultado de la Suma es: " + resultado);
+                }
+                case 2 -> {//Resta
+                    resultado = operando1 - operando2;
+                    System.out.println("El resultado de la Resta es: " + resultado);
+                }
+                case 3 -> {//Multiplicación
+                    resultado = operando1 * operando2;
+                    System.out.println("El resultado de la Multiplicación es: " + resultado);
+                }
+                case 4 -> {//División
+                        resultado = operando1 / operando2;
+                        System.out.println("El resultado de la División es: " + resultado);
+                }
+                default -> System.out.println("Opción erronea: " + operacion);
+            } //Fin del switch
+        } //Fin del if
+        else if (operacion == 5) {
+            System.out.println("Gracias por usar la aplicación");
+        } else {
+            System.out.println("Opción erronea: " + operacion);
         }
-    }
-}
+    } //Fin del método main
+} //Fin de la clase CalculadoraUTN

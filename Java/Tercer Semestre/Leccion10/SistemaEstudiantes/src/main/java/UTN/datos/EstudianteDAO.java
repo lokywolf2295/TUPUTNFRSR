5de3442986546f846b1dbf46ea1a83a9d5a9829f
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-23 22:22:46 -0300

    Update(Java): 3  create an instance of the Conexion class

--- Java/Tercer Semestre/Leccion10/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Tercer Semestre/Leccion10/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -1,7 +1,9 @@
 package UTN.datos;
 
+import static UTN.conexion.Conexion.getConexion;
 import UTN.dominio.Estudiante;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,5 +16,7 @@ public class EstudianteDAO {
         //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos
         PreparedStatement ps; //Envia la sentencia a la base de datos
         ResultSet rs; //Obtenemos el resultado de la consulta
+        Connection con = getConexion(); //Creamos una instancia de la clase Conexion
+        String sql = "SELECT * FROM estudiantes"; //Sentencia SQL que se ejecutará en la base de datos
     }
 }

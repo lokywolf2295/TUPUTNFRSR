008bbfac90a63b3a278dee0e041f339385bc9375
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-23 22:48:52 -0300

    Update(Java): 6 add main method and modify sql script

--- Java/Tercer Semestre/Leccion10/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Tercer Semestre/Leccion10/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -11,13 +11,13 @@ import java.util.List;
 
 public class EstudianteDAO {
     //Metod Listar
-    public List<Estudiante> listar() {
+    public List<Estudiante> listarEstudiantes() {
         List<Estudiante> estudiantes = new ArrayList<>();
         //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos
         PreparedStatement ps; //Envia la sentencia a la base de datos
         ResultSet rs; //Obtenemos el resultado de la consulta
         Connection con = getConexion(); //Creamos una instancia de la clase Conexion
-        String sql = "SELECT * FROM estudiantes ORDER BY estudiantes2022"; //Sentencia SQL que se ejecutará en la base de datos
+        String sql = "SELECT * FROM estudiantes2022 ORDER BY idestudiantes2022"; //Sentencia SQL que se ejecutará en la base de datos
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
@@ -45,4 +45,13 @@ public class EstudianteDAO {
         } //Fin try-catch-finally
         return estudiantes;
     } //Fin metodo listar
+
+    public static void main(String[] args) {
+        //Listar los estudiantes
+        var estudianteDao = new EstudianteDAO();
+        System.out.println("Listado de estudiantes");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println); //Función lambda para imprimir los estudiantes
+    }
+
 } //Fin clase

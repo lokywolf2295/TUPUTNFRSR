e6cbfb3816b1ff77628c2d0fe49427a7d3a52b65
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-26 22:09:42 -0300

    Update(Python 5): 1 add try-except sentence

--- Python/Tercer Semestre/Leccion 05/BD/prueba_bd.py
+++ Python/Tercer Semestre/Leccion 05/BD/prueba_bd.py
@@ -7,15 +7,18 @@ conexion = psycopg2.connect(
     port='5432',
     database='test_bd'
 )
+try:
+    with conexion: # Crear cursor para ejecutar sentencias SQL en PostgreSQL
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia)  # Ejecutar sentencia SQL
+            registros = cursor.fetchall()  # Obtener todos los registros de la consulta
+            print(registros)
 
-#print(conexion)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    # Cerrar cursor
+    conexion.close()
 
-# Crear cursor para ejecutar sentencias SQL en PostgreSQL
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # Ejecutar sentencia SQL
-registros = cursor.fetchall() # Obtener todos los registros de la consulta
-print(registros)
-
-# Cerrar cursor
-cursor.close()
+# https://www.psycopg.org/docs/usage.html

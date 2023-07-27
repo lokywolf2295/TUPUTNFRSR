46adacefdbd4911a45c36db3cd6ce8efd008b530
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-27 00:01:43 -0300

    Update(Python 5): 3 add v_registros file

---
+++ Python/Tercer Semestre/Leccion 05/BD/v_registros.py
@@ -0,0 +1,26 @@
+import psycopg2 # Librería para conectarse a la BD de PostgreSQL
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='root',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion: # Crear cursor para ejecutar sentencias SQL en PostgreSQL
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN (1,2)'
+            # id_persona = input('Digite un numero para id_persona: ')
+            cursor.execute(sentencia)  # Ejecutar sentencia SQL
+            registros = cursor.fetchall()  # Obtener todos los registros de la consulta
+            for registro in registros:
+                print(registro)
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    # Cerrar cursor
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.htm

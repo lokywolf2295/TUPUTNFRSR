75d3ed37338834f8f83b363e0f4a955ad81bb2d4
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-27 22:00:25 -0300

    Update(Python 5): 7 add actualizar_registro file

---
+++ Python/Tercer Semestre/Leccion 05/BD/actualizar_registro.py
@@ -0,0 +1,25 @@
+import psycopg2 # Librería para conectarse a la BD de PostgreSQL
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='root',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion: # Crear cursor para ejecutar sentencias SQL en PostgreSQL
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s' # Placeholder
+            valores = ('Juan Carlos', 'Roldan', 'jcroldan@mail.com', 6) # Es una Tupla
+            cursor.execute(sentencia, valores)  # Ejecutar sentencia SQL
+            registros_actualizados = cursor.rowcount
+            print(f'Los Registros actualizados son: {registros_actualizados}')
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    # Cerrar cursor
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.htm

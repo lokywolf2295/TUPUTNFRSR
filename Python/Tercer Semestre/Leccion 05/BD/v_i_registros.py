efc23d858b851da72249e351cfb1a7306fb27298
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-27 21:45:52 -0300

    Update(Python 5): 6 add v_i_registros file

---
+++ Python/Tercer Semestre/Leccion 05/BD/v_i_registros.py
@@ -0,0 +1,30 @@
+import psycopg2 # Librería para conectarse a la BD de PostgreSQL
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='root',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion: # Crear cursor para ejecutar sentencias SQL en PostgreSQL
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona (nombre, apellido, email)VALUES (%s, %s, %s)' # Placeholder
+            valores = (
+                ('Carlos', 'Lara', 'clara@mail.com'),
+                ('Carmen', 'Pergamino', 'cpergamino@mail.com'),
+                ('Marcelo', 'Carrillo', 'mcarrillo@mail.com')
+            ) # Tupla de tuplas
+            cursor.executemany(sentencia, valores)  # Ejecutar sentencia SQL
+            # conexion.commit() # Confirmar cambios en la BD
+            registros_insertados = cursor.rowcount
+            print(f'Los Registros insertados son: {registros_insertados}')
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    # Cerrar cursor
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.htm

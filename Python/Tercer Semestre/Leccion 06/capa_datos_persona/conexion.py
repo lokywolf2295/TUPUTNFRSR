14f6999c44c62b0b8165c83fb00fff85a14fa029
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-30 16:40:32 -0300

    Update(Python 8): 4 add obtenerConexion method

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
@@ -1,3 +1,8 @@
+import psycopg2 as bd
+from logger_base import log
+import sys
+
+
 class Conexion:
     _DATABASE = 'test_bd'
     _USERNAME = 'postgres'
@@ -6,3 +11,20 @@ class Conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexión exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool {e}')
+                sys.exit()
+        else:
+            return cls._conexion

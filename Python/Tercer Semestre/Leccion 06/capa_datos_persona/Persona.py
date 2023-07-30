e1b4547a15fa969b69eea91297030e11e6745252
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-29 22:35:58 -0300

    Update(Python 8): 2 add main method

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/Persona.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/Persona.py
@@ -1,3 +1,6 @@
+from logger_base import log
+
+
 class Persona:
     def __init__(self, id_persona=None, nombre=None, apellido=None, email=None):
         self._id_persona = id_persona
@@ -45,3 +48,12 @@ class Persona:
     @email.setter
     def email(self, email):
         self._email = email
+
+
+if __name__ == '__main__':
+    persona1 = Persona(1, 'Juan', 'Perez', 'jperez@mail.com')
+    log.debug(persona1)
+    persona2 = Persona(nombre='Jose', apellido='Lopez', email='ljose@mail.com')
+    log.debug(persona2)
+    persona1 = Persona(id_persona=1)
+    log.debug(persona1)

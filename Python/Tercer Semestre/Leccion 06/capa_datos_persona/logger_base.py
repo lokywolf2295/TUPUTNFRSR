4828264233d7107f3f8f7191f7ef5bb8c129d717
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-29 21:47:29 -0300

    Update(Python 7): 4 complete basicConfigs

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/logger_base.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/logger_base.py
@@ -3,8 +3,15 @@ import logging as log
 # docs.python.org/3/howto/logging.html
 # llamamos una configuración básica
 
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
+
 if __name__== '__main__':
-    log.basicConfig(level=log.DEBUG)
     log.debug('Mensaje a nivel debug')
     log.info('Mensaje a nivel info')
     log.warning('Mensaje a nivel warning')

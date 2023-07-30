67b8a85b85f3daaef047b6260a5e656ca5bf3c46
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-30 17:33:10 -0300

    Update(Python 9): 1 add seleccionar method

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
@@ -1,3 +1,7 @@
+from Persona import *
+from conexion import *
+
+
 class personaDAO:
     """"
     DAO significa: Data Access Object
@@ -11,3 +15,16 @@ class personaDAO:
     _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definimos los métodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []  # creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3]) # cada registro equivale a una columna en la tabla
+                    personas.append(persona)
+                return personas

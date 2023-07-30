64dd8654488e71319d371cb1f7e7f3b581db15ed
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-30 17:52:37 -0300

    Update(Python 9): 3 add insertar method and test this

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
@@ -26,12 +26,27 @@ class personaDAO:
                 registros = cursor.fetchall()
                 personas = []  # creamos una lista
                 for registro in registros:
-                    persona = Persona(registro[0], registro[1], registro[2], registro[3]) # cada registro equivale a una columna en la tabla
+                    persona = Persona(registro[0], registro[1], registro[2],
+                                      registro[3])  # cada registro equivale a una columna en la tabla
                     personas.append(persona)
                 return personas
 
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'persona Insertada: {persona}')
+                return cursor.rowcount
+
 
 if __name__ == '__main__':
+    # Insertar un registro
+    persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@mail.com')
+    personas_insertadas = personaDAO.insertar(persona1)
+    log.debug(f'Personas insertadas: {personas_insertadas}')
+
     # Seleccionamos objetos
     personas = personaDAO.seleccionar()
     for persona in personas:

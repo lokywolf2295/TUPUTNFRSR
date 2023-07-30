2051c975e0133c437af5b2bdc76f71190fda5c39
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-29 21:34:13 -0300

    Update(Python 7): 3 add add if method to test file

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/logger_base.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/logger_base.py
@@ -3,4 +3,10 @@ import logging as log
 # docs.python.org/3/howto/logging.html
 # llamamos una configuración básica
 
-log.basicConfig(level=log.DEBUG)
+if __name__== '__main__':
+    log.basicConfig(level=log.DEBUG)
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')

ac4c9cb229c77a99c565834b8c85b0c02ff00715
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-30 18:15:32 -0300

    Update(Python 9): 5 add eliminar method and test this

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
@@ -49,12 +49,26 @@ class personaDAO:
                 log.debug(f'Persona actualizada: {persona}')
                 return cursor.rowcount
 
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados son: {persona}')
+                return cursor.rowcount
+
 
 if __name__ == '__main__':
+    # Eliminar un registro
+    persona1 = Persona(id_persona=12)
+    personas_eliminadas = personaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+
     # Actualizar un registro
-    persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@mail.com')
-    personas_actualizadas = personaDAO.actualizar(persona1)
-    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    # persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@mail.com')
+    # personas_actualizadas = personaDAO.actualizar(persona1)
+    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     # Insertar un registro
     # persona1 = Persona(nombre='Pedro', apellido='Romero', email='promero@mail.com')

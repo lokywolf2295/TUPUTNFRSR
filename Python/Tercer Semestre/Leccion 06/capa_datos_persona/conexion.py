a4c63b2f43ce81a5f8804cb725fcb45a4497cdbb
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-30 23:18:39 -0300

    Update(Python 10): 3 complete obtenerPool method

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
@@ -25,7 +25,20 @@ class Conexion:
     def obtenerPool(cls):
         if cls._pool is None:
             try:
-                cls._pool = pool.SimpleConnectionPool()
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f'Creación del pool exitosa: {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool {e}')
+                sys.exit()
+        else:
+            return cls._pool
 
 
 if __name__ == '__main__':

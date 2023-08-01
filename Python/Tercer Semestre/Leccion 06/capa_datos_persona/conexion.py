3eaee1237805ccfaf563d2a3fcf88832c7432ddf
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-31 22:29:47 -0300

    Update(Python 11): 1 add liberarConexion method

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/conexion.py
@@ -42,6 +42,11 @@ class Conexion:
         else:
             return cls._pool
 
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexión al pool: {conexion}')
+
 
 if __name__ == '__main__':
     conexion1 = Conexion.obtenerConexion()

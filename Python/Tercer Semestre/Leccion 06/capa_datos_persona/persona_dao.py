2a2a4bde78479addab7bb3b01187fd6c581437ed
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-01 22:41:32 -0300

    Update(Python 11): 8 test all methods

--- Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
+++ Python/Tercer Semestre/Leccion 06/capa_datos_persona/persona_dao.py
@@ -58,17 +58,17 @@ class personaDAO:
 
 if __name__ == '__main__':
     # Eliminar un registro
-    # persona1 = Persona(id_persona=12)
-    # personas_eliminadas = personaDAO.eliminar(persona1)
-    # log.debug(f'Personas eliminadas: {personas_eliminadas}')
+    persona1 = Persona(id_persona=26)
+    personas_eliminadas = personaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
 
     # Actualizar un registro
-    # persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@mail.com')
-    # personas_actualizadas = personaDAO.actualizar(persona1)
-    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    persona1 = Persona(1, 'Juan', 'Pena', 'jpena@mail.com')
+    personas_actualizadas = personaDAO.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     # Insertar un registro
-    persona1 = Persona(nombre='Marcos', apellido='Tejada', email='mtejada@mail.com')
+    persona1 = Persona(nombre='Mateo', apellido='Torres', email='mtorres@mail.com')
     personas_insertadas = personaDAO.insertar(persona1)
     log.debug(f'Personas insertadas: {personas_insertadas}')
 

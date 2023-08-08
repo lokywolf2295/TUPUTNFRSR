4e14e04fb0ee2eb54db5caa00bbe98b8857dfc0d
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-07 22:01:11 -0300

    Update(JavaScript 7) 6 add new object to Persona class

--- JavaScript/TercerSemestre/leccion05/05-03-clases.js
+++ JavaScript/TercerSemestre/leccion05/05-03-clases.js
@@ -115,3 +115,6 @@ console.log(persona1.toString());
 console.log(persona2.toString());
 console.log(empleado1.toString());
 console.log(Persona.contadorPersonas);
+let persona3 = new Persona("Carla", "Pertosi");
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);

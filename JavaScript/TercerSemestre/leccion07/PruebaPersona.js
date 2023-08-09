2246f855b1de98667cb4e057e55fa746835de141
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-09 11:37:52 -0300

    Update(JavaScript 9)  6 test Persona class

--- JavaScript/TercerSemestre/leccion07/PruebaPersona.js
+++ JavaScript/TercerSemestre/leccion07/PruebaPersona.js
@@ -38,11 +38,7 @@ class Persona{
     }
 
     toString(){
-        return `
-        ${this._idPersona} 
-        ${this._nombre} 
-        ${this._apellido} 
-        ${this._edad}`;
+        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this._edad}`;
     }
 }
 
@@ -105,3 +101,10 @@ class Cliente extends Persona{
         ${this._fechaRegistro}`;
     }
 }
+
+// Prueba clase Persona
+let persona1 = new Persona('Juan', 'Perez', 32);
+console.log( persona1.toString() );
+
+let persona2 = new Persona('Carla', 'Ortega', 22);
+console.log( persona2.toString() );

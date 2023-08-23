0a859f7280e081fa9ad1fab7809cfb5e904bf692
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-22 21:09:13 -0300

    Feat(Java 1): 1 add SistemaEstudiantes project and eliminarEstudiante method

---
+++ Java/Cuarto Semestre/Leccion 1/estudiantes.sql
@@ -0,0 +1,30 @@
+-- Crear base de datos
+CREATE database estudiantes;
+
+-- Usar la base de datos
+USE estudiantes;
+
+-- Crear la tabla 
+CREATE TABLE `estudiantes`.`estudiantes2022` (
+  `idestudiantes2022` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `apellido` VARCHAR(45) NULL,
+  `telefono` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  PRIMARY KEY (`idestudiantes2022`));
+
+-- Comenzamos con el CRUD: create(insertar), read(leer), update(actualizar), delete(eliminar)
+-- Listar los estudiantes de la tabla
+SELECT * FROM estudiantes.estudiantes2022;
+
+-- Insertar estudiante
+INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "23526362345", "juan@mail.com");
+
+-- Actualizar estudiante
+UPDATE estudiantes2022 SET nombre= "Juan Carlos", apellido="Garcia" WHERE idestudiantes2022=1;
+
+-- Eliminar estudiante
+DELETE FROM estudiantes2022 WHERE idestudiantes2022=1;
+
+-- Para modificar el idestudiantes2022 y comience en 1
+ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;

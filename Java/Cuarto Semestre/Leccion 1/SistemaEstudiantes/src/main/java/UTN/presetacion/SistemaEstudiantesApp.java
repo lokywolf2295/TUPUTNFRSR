2de58910a6a3fa9d6bac421635c42ff341ed5d2f
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-22 23:24:57 -0300

    Update(Java 1): 5 add mostrarMenu and ejecutarOpciones methods

--- Java/Cuarto Semestre/Leccion 1/SistemaEstudiantes/src/main/java/UTN/presetacion/SistemaEstudiantesApp.java
+++ Java/Cuarto Semestre/Leccion 1/SistemaEstudiantes/src/main/java/UTN/presetacion/SistemaEstudiantesApp.java
@@ -12,11 +12,34 @@ public class SistemaEstudiantesApp {
         var estudianteDao = new EstudianteDAO(); //Esta instancia debe hacerse una vez
         while (!salir){
             try{
-                mostrarMenu();//Este será el método que nos devolverá un booleano
+                mostrarMenu(); //Mostramos el menú
+                //Este será el método ejecutarOpciones que nos devolverá un booleano
                 salir = ejecutarOpciones(consola, estudianteDao); //Este método recibe la instancia de la clase servicio
             }catch (Exception e){
                 System.out.println("Ocurrió un error al ejecutar la operación: " + e.getMessage());
             }
-        }
+        }//Fin while
     } //Fin metodo main
+
+    //Método para mostrar el menú
+    public static void mostrarMenu(){
+        System.out.println("""
+        ******* Sistema de Estudiantes *******
+        1. Listar estudiantes
+        2. Buscar estudiante
+        3. Agregar estudiante
+        4. Modificar estudiante
+        5. Eliminar estudiante
+        6. Salir
+        Elija una opción:
+        """);
+    } //Fin metodo mostrarMenu
+
+    //Método para ejecutar las opciones del menú, va a regresar un booleano, ya que es el que
+    //puede modificar la variable salir, si es verdadero, el programa termina
+    public static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDao) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        return salir;
+    } //Fin metodo ejecutarOpciones
 } //Fin clase

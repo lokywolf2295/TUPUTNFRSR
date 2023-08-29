9d7435e840f2dca6d939079df7303493d44a0468
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-28 23:16:41 -0300

    Update(Python 2): 1 add Sistemas_numeracion.py file to numeric system's

---
+++ Python/Cuarto Semestre/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,17 @@
+# Profundizando en los sistemas de Numeración
+
+#Sistema Decimal
+a = 10
+print(f"a decimal: {a}")
+
+#Sistema Binario
+a = 0b1010
+print(f"a binario: {a}")
+
+#Sistema Octal
+a = 0o12
+print(f"a octal: {a}")
+
+#Sistema Hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")

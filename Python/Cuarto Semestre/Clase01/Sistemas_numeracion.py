092cdf85ba31bf9b3afea36c828eb59930ced652
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-28 23:22:36 -0300

    Update(Python 2): 2 add convert to decimal and binary base

--- Python/Cuarto Semestre/Clase01/Sistemas_numeracion.py
+++ Python/Cuarto Semestre/Clase01/Sistemas_numeracion.py
@@ -15,3 +15,11 @@ print(f"a octal: {a}")
 #Sistema Hexadecimal
 a = 0xA
 print(f"a hexadecimal: {a}")
+
+#Base Decimal
+a = int('23', 10)
+print(f"a = base decimal: {a}")
+
+#Base Binaria
+a = int('10111', 2)
+print(f"a = base binario: {a}")

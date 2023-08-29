f98cca252e064e83c3aa2cefe27c60564a5bf63a
lokywolf2295 <matiasesek1995@gmail.com>
2023-08-28 23:34:36 -0300

    Update(Python 2): 4 add tipo_float.py file

---
+++ Python/Cuarto Semestre/Clase01/tipo_float.py
@@ -0,0 +1,11 @@
+# Profundizando en el tipo float
+a = 3.0
+
+# Constructor de tipo float -> puede recibir int y str
+a = float(10) # le pasamos un tipo entero (int)
+a = float('10') # le pasamos un tipo cadena (str)
+print(f"a: {a:.2f}")
+
+# Notación exponencial (valores positivos y negativos)
+a = 3e5
+print(f"a: {a}")

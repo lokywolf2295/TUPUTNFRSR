16d4497ae0703d1573c0ffae833d09a0e5324aff
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-01 23:07:46 -0300

    Update(Node 2) 1 add app2 file

---
+++ Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,4 @@
+const saludar = (nombre) => { // Función de flecha
+    return `Saludos ${nombre}`; //Usamos back tick's
+} // esto es igual a: 'Saludos ' + nombre;
+console.log(saludar('Matías')); 

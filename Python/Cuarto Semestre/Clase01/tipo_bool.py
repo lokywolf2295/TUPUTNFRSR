78281eed700e8dd78d370e87023ab03452c1ffca
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-06 23:46:07 -0300

    Update(Python 3): 2 add tipo_bool.py file

---
+++ Python/Cuarto Semestre/Clase01/tipo_bool.py
@@ -0,0 +1,9 @@
+# Bool contiene los valores de True y False
+# Los tipos numéricos, es falce para el 0 (cero) y True para cualquier otro valor
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, resultado: {resultado}')
+
+valor = 15
+resultado = bool(valor)
+print(f'valor: {valor}, resultado: {resultado}')

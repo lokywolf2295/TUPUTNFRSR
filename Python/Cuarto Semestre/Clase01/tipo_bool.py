2ff7447913397ef49021f28d21514fa6739eb472
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-07 00:11:31 -0300

    Update(Python 3): 6 add type cycles

--- Python/Cuarto Semestre/Clase01/tipo_bool.py
+++ Python/Cuarto Semestre/Clase01/tipo_bool.py
@@ -51,3 +51,11 @@ if bool(''):
     print('Regresa verdadero')
 else:
     print('Regresa falso')
+
+# Ciclos
+variable = 17
+while variable:
+    print('Regresa verdadero')
+    break
+else:
+    print('Regresa falso')

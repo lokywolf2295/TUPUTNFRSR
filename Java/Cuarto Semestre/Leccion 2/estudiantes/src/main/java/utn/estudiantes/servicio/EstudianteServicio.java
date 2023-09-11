871ee434aaa95a0fa4720a9ae87585add144fc56
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-10 23:59:28 -0300

    Update(Java 4): 1 implements all methods in EstudianteServicio class

--- Java/Cuarto Semestre/Leccion 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Cuarto Semestre/Leccion 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,4 +1,34 @@
 package utn.estudiantes.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
+@Service
 public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteServicio estudianteServicio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteServicio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEsTudiante) {
+        return null;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+
+    }
+
+    @Override
+    public void eliminarEstudiante(Integer idEstudiante) {
+
+    }
 }

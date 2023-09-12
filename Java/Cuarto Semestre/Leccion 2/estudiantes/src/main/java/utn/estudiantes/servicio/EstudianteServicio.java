2e4fcd91d45f9c656a91394db5a9c4c80e65395a
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-11 23:33:51 -0300

    Update(Java 4): 2 complete buscarEstudiantePorId method and fix error in listarEstudiantes method

--- Java/Cuarto Semestre/Leccion 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Cuarto Semestre/Leccion 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -3,23 +3,25 @@ package utn.estudiantes.servicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
 @Service
 public class EstudianteServicio implements IEstudianteServicio{
     @Autowired
-    private EstudianteServicio estudianteServicio;
+    private EstudianteRepositorio estudianteRepositorio;
 
     @Override
     public List<Estudiante> listarEstudiantes() {
-        List<Estudiante> estudiantes = estudianteServicio.findAll();
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
         return estudiantes;
     }
 
     @Override
     public Estudiante buscarEstudiantePorId(Integer idEsTudiante) {
-        return null;
+        Estudiante estudiante = estudianteRepositorio.findById(idEsTudiante).orElse(null);
+        return estudiante;
     }
 
     @Override

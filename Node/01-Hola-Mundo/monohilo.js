ad41992896fc1442e9add499151895a258bbfced
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-23 22:48:42 -0300

    Update(Node 2) 4 finally monohilo.js file

--- Node/01-Hola-Mundo/monohilo.js
+++ Node/01-Hola-Mundo/monohilo.js
@@ -6,8 +6,10 @@ setInterval(function()  {
     console.log(i);
     i++;
 
-    if (i === 5) {
-        console.log('Forzamos error');
-        var a = 3 + z;
-    }
+    // if (i === 5) {
+    //     console.log('Forzamos error');
+    //     var a = 3 + z;
+    // }
 }, 1000);
+
+console.log('Segunda instrucción');

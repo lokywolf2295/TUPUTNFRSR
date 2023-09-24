350415102902df1d14ede4ebea94c5b70a88470a
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-23 23:22:34 -0300

    Update(Node 6) 2 modify entorno.js file

--- Node/01-Hola-Mundo/entorno.js
+++ Node/01-Hola-Mundo/entorno.js
@@ -1,4 +1,8 @@
 
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || 'Sin nombre'; //si no existe el nombre, se le asigna el valor de 'Sin nombre' por defecto
+let web = process.env.WEB || 'No tengo web';
 
-console.log('Hola ' + nombre);
+console.log('Hola ' + nombre); 
+//como primera vez tenemos que agregar el nombre en la terminal de la siguiente manera:
+// $env:NOMBRE="Carlos";
+console.log('Mi web es: ' + web);

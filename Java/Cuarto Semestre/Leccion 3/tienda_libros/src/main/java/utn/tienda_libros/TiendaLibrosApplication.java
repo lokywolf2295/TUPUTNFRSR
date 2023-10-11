b4dd8f7b749706b7daea465ad1503211cbcb0463
lokywolf2295 <matiasesek1995@gmail.com>
2023-10-10 23:09:58 -0300

    Update(Java 8): 2 modify main class

--- Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,9 +1,13 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroForm;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
@@ -11,6 +15,15 @@ public class TiendaLibrosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
-		ConfigurableBootstrapContext contextoSpring =
+		ConfigurableApplicationContext contextoSpring =
 				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		//Ejecitamos el código para cargar el formulario
+		EventQueue.invokeLater(() -> {
+			//Obtenemos el objeto form a través del spring
+			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+		});
 }

1f4c5e96cec50d7873100e9f2db7a75a5a870d3f
lokywolf2295 <matiasesek1995@gmail.com>
2023-10-10 22:57:32 -0300

    Update(Java 8): 1 add Toolkit in LibroForm class and ConfigurableBootstrap.. object in main class

--- Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -1,11 +1,14 @@
 package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import java.awt.*;
 
 
+@Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
@@ -21,5 +24,11 @@ public class LibroForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
+        //Para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()) / 2;
+        int y = (tamanioPantalla.height - getHeight()) / 2;
+        setLocation(x, y);
     }
 }

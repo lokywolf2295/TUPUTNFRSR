b6c54571755b313d8397f448ccbaf02a1fdfdefc
lokywolf2295 <matiasesek1995@gmail.com>
2023-10-10 23:15:00 -0300

    Update(Java 8): 3 fix errors in main class and test app

--- Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Cuarto Semestre/Leccion 3/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,6 +1,5 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,8 +12,7 @@ import java.awt.*;
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+
 		ConfigurableApplicationContext contextoSpring =
 				new SpringApplicationBuilder(TiendaLibrosApplication.class)
 						.headless(false)
@@ -22,8 +20,11 @@ public class TiendaLibrosApplication {
 						.run(args);
 
 		//Ejecitamos el código para cargar el formulario
-		EventQueue.invokeLater(() -> {
+		EventQueue.invokeLater(() -> { //Método Lambda
 			//Obtenemos el objeto form a través del spring
 			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+			libroForm.setVisible(true);
 		});
+	}
+
 }
